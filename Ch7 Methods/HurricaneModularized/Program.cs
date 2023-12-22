4b25f10df5b3f1f346ae16a51226f32ca4a805a0
estmaria <114110218+estmaria@users.noreply.github.com>
2023-12-22 12:21:14 -0500

    Add files via upload

---
+++ Ch7 Methods/HurricaneModularized/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* 
+   Author: Maria Esteban
+   Date Modified: Oct 11 2023
+   Description: prompts the uuser for the wind speed of the hurricane and classifies it into 
+   five categories using the Saffir-Simpson Hurricane Scale
+*/
+namespace HurricaneModularized
+{
+    class Program
+    {
+        public static void CalcCategory(double windSpeed)
+        {
+            string category;
+            if (windSpeed >= 157)
+                category = "is catergory 5";
+            else if (windSpeed >= 130)
+                category = "is catergory 4";
+            else if (windSpeed >= 111)
+                category = "is catergory 3";
+            else if (windSpeed >= 96)
+                category = "is catergory 2";
+            else if (windSpeed >= 74)
+                category = "is catergory 1";
+            else
+                category = "is not a hurricane";
+
+            WriteLine("This hurricane {0}", category);
+        }
+        static void Main(string[] args)
+        {
+            Write("Enter a wind speed in mph: ");
+            CalcCategory(Convert.ToDouble(ReadLine()));
+        }
+    }
+}
---
+++ Ch7 Methods/PaintingEstimate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 
+   Author: Maria Esteban
+   Date Modified: Oct 11 2023
+   Description:  prompts a user for length and width of a room in feet and calculates how much would it cost to paint that room
+*/
+
+
+namespace PaintingEstimate
+{
+    class Program
+    {
+        public static double CalcPrice(double width, double length)
+        {
+            const int HEIGHT= 9;
+            const int PRICE_PER_SQUARE_FOOT = 6;
+            return PRICE_PER_SQUARE_FOOT*(2*length*HEIGHT+2*width*HEIGHT);
+        }
+        static void Main(string[] args)
+        {
+            Write("Enter width in feet: ");
+            double width = Convert.ToInt32(ReadLine());
+            Write("Enter length in feet: ");
+            double length = Convert.ToInt32(ReadLine());
+            WriteLine("The total pricce for this room is {0}", (CalcPrice(length, width)).ToString("C"));
+        }
+    }
+}
